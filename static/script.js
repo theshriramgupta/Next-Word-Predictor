dff369420830660af8a4a01bc5b75cdd1b045134
Shriram Gupta <71961083+theshriramgupta@users.noreply.github.com>
2025-03-16 12:49:24 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
-
+async function predictWord() {
+  let text = document.getElementById("user_input").value;
+  let response = await fetch("/predict", {
+    method: "POST",
+    headers: {"Content-Type" : "application/json"},
+    body: JSON.stringify({text : text, num_words: 1})
+  });
+  let data = await response.json();
+  document.getElementById("predictedResult").innerText = data.predicted_text;
+}
