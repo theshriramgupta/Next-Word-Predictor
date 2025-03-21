20b24740ccd93e5a752619947ac29a469545fefc
Shriram Gupta <71961083+theshriramgupta@users.noreply.github.com>
2025-03-21 10:52:04 +0530

    Add meaning and correctSentence feature

--- static/script.js
+++ static/script.js
@@ -1,10 +1,32 @@
 async function predictWord() {
-  let text = document.getElementById("user_input").value;
-  let response = await fetch("/predict", {
-    method: "POST",
-    headers: {"Content-Type" : "application/json"},
-    body: JSON.stringify({text : text, num_words: 1})
-  });
-  let data = await response.json();
-  document.getElementById("predictedResult").innerText = data.predicted_text;
+    let text = document.getElementById("predictInput").value;
+    let response = await fetch("/predict", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: text, num_words: 1 })
+    });
+    let data = await response.json();
+    document.getElementById("predictionResult").innerText = data.predicted_text;
+}
+
+async function findMeaning() {
+    let word = document.getElementById("meaningInput").value;
+    let response = await fetch("/meaning", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ word: word })
+    });
+    let data = await response.json();
+    document.getElementById("meaningResult").innerText = data.meaning;
+}
+
+async function correctSentence() {
+    let sentence = document.getElementById("correctInput").value;
+    let response = await fetch("/correct", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ sentence: sentence })
+    });
+    let data = await response.json();
+    document.getElementById("correctedResult").innerText = data.corrected_sentence;
 }
